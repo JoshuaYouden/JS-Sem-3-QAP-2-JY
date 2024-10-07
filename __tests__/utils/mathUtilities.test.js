150a16181b9d54ece31943efc1ab80d4073e8b6f
Joshua Youden <Joshua.Youden@keyin.com>
2024-10-07 14:31:21 -0230

    Base done, Tests done, and Math Functions also done.

--- __tests__/utils/mathUtilities.test.js
+++ __tests__/utils/mathUtilities.test.js
@@ -1,9 +1,19 @@
 const { isCorrectAnswer, getQuestion } = require("../../utils/mathUtilities");
 
 describe("Tests for getQuestion", () => {
-
+  test("Gets a random multiplication, division, subtraction or addition question", () => {
+    expect(getQuestion()).toBeTruthy();
+  });
 });
 
 describe("Tests for isCorrectAnswer", () => {
-    
+  test("Parses the provided question and gets whether or not the provided answer is correct", () => {
+    expect(isCorrectAnswer()).toBeTruthy();
+  });
+});
+
+describe("Tests for incorrectAnswer", () => {
+  test("Parses the provided question and gets whether or not the provided answer is incorrect", () => {
+    expect(isCorrectAnswer()).toBeFalsy();
+  });
 });
--- utils/mathUtilities.js
+++ utils/mathUtilities.js
@@ -1,24 +1,97 @@
+const problems = [
+  {
+    problem: "10 + 25",
+    answer: 35,
+  },
+  {
+    problem: "100 + 50",
+    answer: 150,
+  },
+  {
+    problem: "45 + 25",
+    answer: 70,
+  },
+  {
+    problem: "25 + 25",
+    answer: 50,
+  },
+  {
+    problem: "10 - 25",
+    answer: -15,
+  },
+  {
+    problem: "100 - 50",
+    answer: 50,
+  },
+  {
+    problem: "45 - 25",
+    answer: 20,
+  },
+  {
+    problem: "25 - 25",
+    answer: 0,
+  },
+  {
+    problem: "10 * 25",
+    answer: 250,
+  },
+  {
+    problem: "25 * 2",
+    answer: 50,
+  },
+  {
+    problem: "10 * 10",
+    answer: 100,
+  },
+  {
+    problem: "10 * 5",
+    answer: 50,
+  },
+  {
+    problem: "10 / 25",
+    answer: 0.4,
+  },
+  {
+    problem: "150 / 10",
+    answer: 15,
+  },
+  {
+    problem: "35 / 5",
+    answer: 7,
+  },
+  {
+    problem: "100 / 20",
+    answer: 5,
+  },
+];
+
 /**
  * Gets a random multiplication, division, subtraction or addition question
- * 
- * @returns {} The randomly generated math question
+ *
+ * @returns {object} The randomly generated math question
  */
-function getQuestion() {
 
+function getQuestion() {
+  const question = Math.floor(Math.random() * problems.length);
+  return problems[question];
 }
 
 /**
  * Parses the provided question and gets whether or not the provided answer is correct
- * 
+ *
  * @param {*} question The question being answered
  * @param {*} answer The potential answer
  * @returns {boolean} True if the answer was correct, false otherwise.
  */
 function isCorrectAnswer(question, answer) {
+  if (question.answer === answer) {
+    return true;
+  } else {
     return false;
+  }
 }
 
 module.exports = {
-    getQuestion,
-    isCorrectAnswer
-}
+  getQuestion,
+  isCorrectAnswer,
+};
